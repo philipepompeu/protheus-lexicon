bc89471286c5e7e0f4980b8b40e9937966f4aa5d
philipepompeu <philipepompeu2@gmail.com>
2025-01-28 15:33:27 -0300

    Initial setup for frontend with Vue.js and Vuetify

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>Protheus Lexicon</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-card class="mx-auto" max-width="400">
+          <v-card-title>Bem-vindo ao Protheus Lexicon</v-card-title>
+          <v-card-text>
+            Esta aplicação permite consultar e comparar os dicionários do Protheus.
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
