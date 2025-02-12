e6497eed66ee375a4a531affca1a9bafe6bd8eae
philipepompeu <philipepompeu2@gmail.com>
2025-02-12 17:46:51 -0300

    listagem dos grupos de perguntas

---
+++ backend/protheus-lexicon-backend/src/main/resources/script/exporta-postgres.sh
@@ -0,0 +1,6 @@
+
+#exporta apenas os schemas
+pg_restore --no-owner --no-privileges --table=sx3t10 --table=sx2t10 --table=sx6t10 --table=sx7t10 --table=sixt10 --table=sx1t10 --clean --create --schema-only -v -f v1_create_database.sql p1212410mntdbexp.dump
+
+#exporta apenas os dados
+pg_restore --no-owner --no-privileges --table=sx3t10 --table=sx2t10 --table=sx6t10 --table=sx7t10 --table=sixt10 --table=sx1t10 --data-only -v -f V2__populate_database.sql p1212410mntdbexp.dump
--- frontend/src/components/QuestionsList.vue
+++ frontend/src/components/QuestionsList.vue
@@ -5,7 +5,26 @@
           <v-progress-circular indeterminate color="primary"></v-progress-circular>
           Carregando perguntas...
         </v-col>
-        <v-col v-else cols="12">
+        <v-col v-else cols="12">       
+
+          <v-dialog v-model="selectedGroup" max-width="500">
+            <v-card v-if="selectedGroup">
+              <v-card-title>{{ selectedGroup.name }}</v-card-title>
+              <v-card-text>
+                <v-list density="compact">
+                  <v-list-item
+                    v-for="item in selectedGroup.items"
+                    :key="item.id"
+                    :title="`${item.sequence} - ${item.questionText}`"
+                  ></v-list-item>
+                </v-list>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn @click="selectedGroup = null">Fechar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
           <v-data-table-server
             v-model:items-per-page="itemsPerPage"
             :search="filterByName"
@@ -13,6 +32,7 @@
             :headers="headers"
             :items="questions"
             :loading="loading"
+            hover="true"
             @update:options="fetchQuestions"
             item-value="id"
             class="elevation-1"
@@ -21,40 +41,29 @@
               <v-toolbar flat>
                 <v-toolbar-title>Perguntas(SX1)</v-toolbar-title>
               </v-toolbar>
-            </template>
+            </template>            
   
-            <template v-slot:[`item.name`]="{ item }">
-                <v-list lines="one" density="compact">
-                    <v-list-item
-                        v-for="question in item.items"
-                        :key="`${question.id}`"
-                        :title="`${question.sequence}`"
-                        :subtitle= "`${question.questionText}`"
-                    ></v-list-item>
-                </v-list>
-              <!--<router-link :to="`/questions/${item.name}`" class="text-decoration-none">
+            <template v-slot:[`item.name`]="{ item }" >            
+              
 
+              <v-chip color="primary" text-color="white" @click="selectedGroup = item">
+                {{ item.name }}
+              </v-chip>
+              <v-badge color="info" :content="item.items.length" inline></v-badge>
 
-                <v-chip color="primary" text-color="white">
-                  {{ item.name }}
-                </v-chip>                
-              </router-link> -->             
-             
             </template>
-  
-            <!--template v-slot:[`item.primaryKey`]="{ item }">
-              <span>{{ item.primaryKey }}</span>
-            </template>-->
 
-            <template v-slot:tfoot>
+            <!--<template v-slot:tfoot>
               <tr>
                 <td>
                   <v-text-field v-model="filterByName" class="ma-2" density="compact" placeholder="Buscar grupo..." hide-details></v-text-field>
                 </td>                
               </tr>
-            </template>
+            </template>-->
   
-          </v-data-table-server>
+          </v-data-table-server> 
+
+
         </v-col>
       </v-row>
     </v-container>
@@ -62,7 +71,8 @@
   
   <script>
   import  questionService from '../services/questionService';
-  
+  import { ref } from "vue";
+
   export default {
     name: 'questionsList',
     data() {
@@ -72,8 +82,9 @@
         totalItems: 0,
         //page:0,
         itemsPerPage: 10,
+        selectedGroup : ref(null),
         headers: [
-          { title: 'Nome', align: 'start', key: 'name', sortable: true }
+          { title: 'Grupo', align: 'start', key: 'name', sortable: true }
         ],
         filterByName: ''
       };
