8e8cdcbb5d10e693dbb6b37092be3da02a8aa91a
philipepompeu <philipepompeu2@gmail.com>
2025-02-24 13:58:24 -0300

    correção nas redes entre os containers

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 // src/config.js
-export const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:8080'; // Alterar conforme necessário
+export const API_URL = process.env.VUE_APP_API_URL || '/api'; // Alterar conforme necessário
